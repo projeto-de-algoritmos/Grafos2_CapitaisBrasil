da769826e27e41a181639f3e8855ab990e10675b
Laura <62102447+laurapinos@users.noreply.github.com>
2022-12-05 21:59:30 -0300

    Organização do arquivo do grafo

--- src/grafo.py
+++ src/grafo.py
@@ -1,6 +1,3 @@
-from math import inf
-
-
 class Grafo(object):
     """ Implementação da estrutura de um grafo. """
 
@@ -46,85 +43,40 @@ class Grafo(object):
         return self.grafo[node1][node2]
 
 
-def dijkstra(grafo: Grafo, estado_partida):
-    vertices_nao_visitados = list(grafo.get_vertices())
+# Distância entre as capitais em km/1000
+adjacencias = {
+    "AC": {"AM": 1.44, "RO": 0.54},
+    "AM": {"RO": 0.9, "MT": 2.35, "PA": 5.29, "RR": 0.78},
+    "RO": {"MT": 1.45},
+    "RR": {"PA": 6.08},
+    "PA": {"TO": 1.28, "MT": 2.94, "AP": 1.91, "MA": 0.8},
+    "TO": {"PI": 1.4, "MT": 1.78, "BA": 1.45, "GO": 0.87, "MA": 1.38},
+    "MA": {"PI": 0.44},
+    "PI": {"PE": 1.13, "BA": 1.16, "CE": 0.63},
+    "BA": {"PE": 0.83, "MG": 1.37, "GO": 1.64, "ES": 1.2, "SE": 0.35, "AL": 0.63},
+    "CE": {"PE": 0.8, "PB": 0.68, "RN": 0.53},
+    "RN": {"PB": 0.18},
+    "PB": {"PE": 0.12},
+    "PE": {"AL": 0.28},
+    "AL": {"SE": 0.29},
+    "SE": {},  # já teve todos os seus vizinhos anotados anteriormente
+    "MT": {"GO": 0.93, "MS": 0.69},
+    "GO": {"DF": 0.2, "MS": 0.93, "MG": 0.9},
+    "DF": {"MG": 0.71},
+    "MS": {"MG": 1.45, "SP": 1.01, "PR": 0.99},
+    "MG": {"SP": 0.58, "ES": 0.52, "RJ": 0.43},
+    "ES": {"RJ": 0.52},
+    "RJ": {"SP": 0.42},
+    "SP": {"PR": 0.40},
+    "PR": {"SC": 0.70},
+    "SC": {"RS": 0.47},
+    "RS": {},  # já teve todos os seus vizinhos anotados anteriormente
+}
 
-    menor_caminho = {}
-    estados_do_caminho = {}
-
-    # Inicializa todos os nós não visitados com valor infinito.
-    for node in vertices_nao_visitados:
-        menor_caminho[node] = inf
-    # O nó inicial começa com o valor 0
-    menor_caminho[estado_partida] = 0
-
-    while vertices_nao_visitados:
-        menor_vertice_atual = None
-        # Encontrar o vértice com o menor peso
-        for node in vertices_nao_visitados:
-            if menor_vertice_atual is None:
-                menor_vertice_atual = node
-            elif menor_caminho[node] < menor_caminho[menor_vertice_atual]:
-                menor_vertice_atual = node
-
-        # Atualiza os valores das distâncias entre um nó e outro
-        vizinhos_vertice_atual = grafo.get_vizinhos(menor_vertice_atual)
-        for vizinho in vizinhos_vertice_atual:
-            peso_temporario = menor_caminho[menor_vertice_atual] + grafo.peso_aresta(menor_vertice_atual, vizinho)
-            if peso_temporario < menor_caminho[vizinho]:
-                menor_caminho[vizinho] = peso_temporario
-                # Atualiza o melhor caminho para o nó sendo visitado
-                estados_do_caminho[vizinho] = menor_vertice_atual
-
-        vertices_nao_visitados.remove(menor_vertice_atual)
-    return estados_do_caminho, menor_caminho
-
-
-def mensagem(nos_do_caminho, menor_caminho, estado_partida, estado_chegada):
-    estados = {
-        'AC': 'Acre', 'AL': 'Alagoas', 'AM': 'Amazonas', 'AP': 'Amapá',
-        'BA': 'Bahia', 'CE': 'Ceará', 'DF': 'Distrito Federal', 'ES': 'Espírito Santo',
-        'GO': 'Goiás', 'MA': 'Maranhão', 'MG': 'Minas Gerais', 'MS': 'Mato Grosso do Sul',
-        'MT': 'Mato Grosso', 'PA': 'Pará', 'PB': 'Paraíba', 'PE': 'Pernambuco', 'PI': 'Piauí',
-        'PR': 'Paraná', 'RJ': 'Rio de Janeiro', 'RN': 'Rio Grande do Norte', 'RO': 'Rondônia',
-        'RR': 'Roraima', 'RS': 'Rio Grande do Sul', 'SC': 'Santa Catarina', 'SE': 'Sergipe',
-        'SP': 'São Paulo', 'TO': 'Tocantins'
-    }
-
-    caminho = []
-    node = estado_chegada
-    while node != estado_partida:
-        caminho.append(node)
-        node = nos_do_caminho[node]
-
-    # Adicionando o nó (estado) inicial manualmente ao final da lista
-    caminho.append(estado_partida)
-
-    estado = [estados[i] for i in caminho]
-    print(f"A menor distância entre as capitais de {estado[-1]} e {estado[0]} é: {menor_caminho[estado_chegada]} km.")
-    print(" -> ".join(reversed(estado)))
+siglas = ['AC', 'AM', 'RO', 'RR', 'PA', 'AP', 'TO', 'MA', 'PI', 'BA', 'CE', 'RN', 'PB',
+          'PE', 'AL', 'SE', 'MT', 'GO', 'MS', 'DF', 'MG', 'ES', 'SP', 'RJ', 'PR', 'SC', 'RS']
 
 
 if __name__ == '__main__':
+    grafo = Grafo(siglas, adjacencias)
 
-    adjacencias = {
-        "AC": {"AM": 2, "RO": 3},
-        "AM": {"RO": 9, "MT": 6},
-        "RO": {"AM": 5, "MT": 1, "AC": 4, "PR": 5},
-        "MT": {"TO": 5, "RO": 4, "GO": 2, "PA": 1, "MA": 8, "MS": 1},
-        "MA": {"TO": 5, "PA": 3, "PI": 1, "PR": 4},
-        "PR": {"SP": 1, "SC": 2, "MS": 4},
-        "RR": {"AM": 5, "PA": 1},
-        "RN": {"CE": 2, "PB": 2},
-        "SC": {"RS": 3, "PR": 2},
-        "RS": {"SC": 5},
-    }
-
-    estados = ['AC', 'AM', 'RO', 'RR', 'PA', 'AP', 'TO', 'MA', 'PI', 'BA', 'CE', 'RN', 'PB', 'PE', 'AL', 'SE', 'MT',
-    'GO', 'MS', 'DF', 'MG', 'ES', 'SP', 'RJ', 'PR', 'SC', 'RS']
-
-    grafo = Grafo(estados, adjacencias)
-
-    estados_do_caminho, menor_caminho = dijkstra(grafo=grafo, estado_partida="AC")
-
-    mensagem(estados_do_caminho, menor_caminho, estado_partida="AC", estado_chegada="MS")
